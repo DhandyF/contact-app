f5849da81eb76d17fff48ee526100534b4a12cc9
moch dhandy friyadi <moch.dhandy@lawencon.com>
2022-11-14 23:06:50 +0700

    add delete function

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -2,15 +2,41 @@ import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
 
-function ContactApp() {
-  const contacts = getData();
+class ContactApp extends React.Component {
+  constructor({ props }) {
+    super(props);
 
-  return (
-    <div className='contact-app'>
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} />
-    </div>
-  );
+    this.state = {
+      contacts: getData()
+    }
+
+    this.onDeleteEventListener = this.onDeleteEventListener.bind(this);
+  }
+
+  onDeleteEventListener(id) {
+    const contacts = this.state.contacts.filter((contact) => contact.id !== id);
+    this.setState({ contacts });
+  }
+  
+  render() {
+    return (
+      <div className='contact-app'>
+        <h1>Daftar Kontak</h1>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteEventListener} />
+      </div>
+    )
+  }
 }
+
+// function ContactApp() {
+//   const contacts = getData();
+
+//   return (
+//     <div className='contact-app'>
+//       <h1>Daftar Kontak</h1>
+//       <ContactList contacts={contacts} />
+//     </div>
+//   );
+// }
 
 export default ContactApp;
